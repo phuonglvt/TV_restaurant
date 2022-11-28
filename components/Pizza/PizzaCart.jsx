9651e9b804bd52ec58e54c5ce892f70db472cb83
phuonglvt <phuongtrucluu12082000@gmail.com>
2022-11-28 20:13:16 +0700

    fix pizza api

--- components/Pizza/PizzaCart.jsx
+++ components/Pizza/PizzaCart.jsx
@@ -2,19 +2,19 @@ import styles from "../../styles/Pizza/PizzaCart.module.css";
 import pizza1 from "../../pages/images/pizza.jpeg";
 import Image from "next/image";
 // import Image from "react";
+// import React from "react";
 
 const PizzaCart = ({pizza}) => {
     return (
         <div className={styles.container}>
             
-            <Image src={require (pizza.img)} alt="" width="300" height="200"/>
+            {/* <Image src={require ("/pages/images/pizza.jpeg")} alt="" width="300" height="200"/> */}
 
             {/* <img src={require (pizza.img)} alt="" width="300" height="200"/> */}
-            {/* <img src={require("../../pages/images/pizza.jpeg")} alt="" width="300" height="200"/> */}
-            <h1 className={styles.title}>{pizza.img}</h1> 
+            <img src={require (pizza.img)} alt="" width="300" height="200"/>
             <h1 className={styles.title}>{pizza.title}</h1>
             <span className={styles.price}>{pizza.prices[0]}vnd</span>
-            <p className={styles.desc}> 
+            <p className={styles.desc}>
                 {pizza.desc}
             </p>
         </div>
